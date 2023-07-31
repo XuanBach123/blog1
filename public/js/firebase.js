441c28982a2ae015ebdcb0708499dc0b9cebbb18
XuanBach123 <hobachthcsbth2016@gmail.com>
2023-07-31 14:37:00 +0700

    Add file

---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyCzhRtkJWkUTmS5tP8TnJPod_nN_e9lZco",
+  authDomain: "bloggin-website-2517a.firebaseapp.com",
+  projectId: "bloggin-website-2517a",
+  storageBucket: "bloggin-website-2517a.appspot.com",
+  messagingSenderId: "974756874472",
+  appId: "1:974756874472:web:12859b10254545d954215d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
